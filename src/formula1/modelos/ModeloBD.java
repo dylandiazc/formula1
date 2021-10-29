6b27f821b29029c31bd58dfa30bf8bc3d6cd53da
dylandiazc <71187104+dylandiazc@users.noreply.github.com>
2021-10-29 07:52:09 -0500

    se crea modelo

---
+++ src/formula1/modelos/ModeloBD.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formula1.modelos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author 57305
+ */
+public class ModeloBD {
+    
+    //donde vive la base de datos 
+    private String servidorBD="jdbc:mysql://localhost/formula1";
+    private String usuarioBD="root";
+    private String passwordBD="";
+
+    public ModeloBD() {
+    }
+    
+    public Connection conectar(){
+        
+        Connection conexion=null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion=DriverManager.getConnection(servidorBD,usuarioBD,passwordBD);
+            System.out.println("Éxito en la conexión");
+            return conexion;
+            
+        }catch(Exception error){
+            System.out.println("uyyy.. "+error);
+            return null;
+        }
+        
+    }
+    
+}
